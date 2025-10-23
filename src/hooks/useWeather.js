a36c0c11524964c2829f1a31e3b2d8db398dcaea
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-10-23 21:39:37 +0300

    added hooks, error handling, guard clause

---
+++ src/hooks/useWeather.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+import { getCurrentWeather, getForecast } from '../utils/weatherApi';
+
+export const useWeather = (city) => {
+	const [weather, setWeather] = useState(null);
+	const [forecast, setForecast] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		if (!city) {
+			setLoading(false);
+			return;
+		}
+
+		const fetchWeatherData = async () => {
+			setLoading(true);
+			setError(null);
+			try {
+				const [CurrentWeatherData, forecastData] = await Promise.all([
+					getCurrentWeather(city),
+					getForecast(city),
+				]);
+
+				setWeather(CurrentWeatherData);
+				setForecast(forecastData);
+			} catch (error) {
+				setError(error.message);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		fetchWeatherData();
+	}, [city]);
+
+	return { weather, forecast, loading, error };
+};
